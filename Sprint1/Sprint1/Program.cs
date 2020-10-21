83704e848a0cd973ad8249505c575cbf76f6eb5a
Lennart-Meyer <lennart.meyer@student.jade-hs.de>
2020-10-21 10:01:07 +0200

    Update Program.cs

--- Sprint1/Sprint1/Program.cs
+++ Sprint1/Sprint1/Program.cs
@@ -10,7 +10,64 @@ namespace Sprint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Es klappt");
+            Double m;  //m=Modul
+            Double p;  //p=Teilung
+            Double d;  //d=Teilkreisdurchmesser
+            Double da; //da=Kopfkreisdurchmesser
+            Double df; //df=Frußkreisdurchmesser
+            Double z;  //z=Zähnezahl
+            Double h;  //h=Zahnhöhe
+            Double ha; //ha=Zahnkopfhöhe
+            Double hf; //hf=Zahnfußhöhe
+            Double c;  //c=Kopfspiel
+            Double a;  //a=Achsenabstand
+
+            Console.WriteLine("Wollen sie die Modul und Teilkreisdurchmesser vorgeben press 1");
+            Console.WriteLine("Wollen sie die Msduodul und Zähnezahl vorgeben press 2");
+            Console.WriteLine("Wollen sie die Zähnezahl und Teilkreisdurchmesser vorgeben press 3");
+            Console.WriteLine("und bestätigen sie ihre Eingabe mit Enter");
+
+            int ii =
+                Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (ii == 1)
+
+            {
+                Console.WriteLine("Bitte geben sie das Modul ein.");
+                m = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben sie den Teilkreisdurchmesser in mm an");
+                d = Convert.ToDouble(Console.ReadLine());
+                z = d / m;
+                ii = ii + 5;
+            }
+
+            else if (ii == 2)
+
+            {
+                Console.WriteLine("Bitte geben sie das Modul ein.");
+                m = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben sie den Teilkreisdurchmesser in mm an");
+                d = Convert.ToDouble(Console.ReadLine());
+                
+                ii = ii + 5;
+            }
+
+
+
+            Console.WriteLine(ii);
+            Console.WriteLine(m);
+            Console.WriteLine(z);
+            Console.WriteLine(d);
+
+
+
+
+
+
+
+
             Console.ReadLine();
 
 
